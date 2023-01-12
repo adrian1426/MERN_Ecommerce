67e32a786ce8f257cece07be471dd4ea8307d365
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-01-12 14:16:14 -0600

    backend - buenas practicas, agregando env

--- ecomm-backend/server.js
+++ ecomm-backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express');
+require('dotenv').config();
 
-const PORT = 8080;
+const express = require('express');
+const { appConfig } = require('./config');
+
+const PORT = appConfig.port;
 const app = express();
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
