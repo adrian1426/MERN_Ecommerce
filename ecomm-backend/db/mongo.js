d64e8fcad099d083915948a3f84c71d24854f3ea
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-01-12 19:56:45 -0600

    catcheando errores de conexión a db

--- ecomm-backend/db/mongo.js
+++ ecomm-backend/db/mongo.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
+mongoose.connection.on('open', () => console.log('Conected to db'));
+
 async function connectDB({ host, port, dbName }) {
   const uri = `mongodb://${host}:${port}/${dbName}`;
   mongoose.set('strictQuery', false);
-  mongoose.connect(uri, { useNewUrlParser: true });
+  await mongoose.connect(uri, { useNewUrlParser: true });
 }
 
 module.exports = connectDB;
--- ecomm-backend/server.js
+++ ecomm-backend/server.js
@@ -7,8 +7,18 @@ const connectMongoDB = require('./db/mongo');
 const PORT = appConfig.port;
 const app = express();
 
-connectMongoDB(dbConfig)
-  .then(() => console.log('Conectado a BD'))
-  .catch(err => console.log('Error de conexión a bd, ', err));
+// connectMongoDB(dbConfig)
+//   .then(() => console.log('Conectado a BD'))
+//   .catch(err => console.log('Error de conexión a bd, ', err));
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+async function initApp(dbConfig, PORT) {
+  try {
+    await connectMongoDB(dbConfig);
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+}
+
+initApp(dbConfig, PORT);
