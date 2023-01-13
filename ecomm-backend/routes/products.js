b5e079ad8e1d3a178f38598c9791324df9a21b4f
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-01-13 13:35:28 -0600

    subiendo archivos a node

--- ecomm-backend/routes/products.js
+++ ecomm-backend/routes/products.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { addProduct } = require('../controllers/productController');
+const upload = require('../libs/storage');
 
 const api = express.Router();
 
-api.post('/products', addProduct);
+api.post('/products', upload.single('imagen'), addProduct);
 
 module.exports = api;
