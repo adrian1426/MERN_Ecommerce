0804f19e3d082a250e148e1381193bb8bc1590fb
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-01-17 18:47:01 -0600

    renderizado condicional

--- ecomm-frontend/src/Products/components/ListProduct.js
+++ ecomm-frontend/src/Products/components/ListProduct.js
@@ -3,7 +3,7 @@ import { getProducts } from '../services';
 import Loading from './Loading';
 
 const ListProduct = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
   const loadProducts = async () => {
@@ -12,15 +12,25 @@ const ListProduct = () => {
     if (resProducts.status === 200) {
       setProducts(resProducts.data.products);
     }
+
+    setIsLoading(false);
   };
 
   useEffect(() => {
     loadProducts();
   }, []);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (products.length === 0) {
+    return <h2 className="title has-text-centered">You don't have any products</h2>;
+  }
+
   return (
     <div>
-      <Loading />
+      <h2>Listado de productos</h2>
     </div>
   );
 };
